b4b7ba30f73b5cd9b25205f27fae80852b2ab05c
RafaPizza3 <rafapitherpizza@gmail.com>
2025-09-26 07:26:05 -0300

    exibindo versão do app no layout

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return (
         '<h1>Olá, Mundo! Imagem Docker publicada com sucesso via GitHub Actions!</h1>'
-        '<p>Minha primeira aplicação Flask em um container Docker.</p>')
+        '<p>Minha primeira aplicação Flask em um container Docker.</p>'
+        '<h2>Versão: 0.0.3<h2>'
+        )
 
 if __name__ == '__main__':
     
